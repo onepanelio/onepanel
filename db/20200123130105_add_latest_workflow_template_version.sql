99ecbb5796a1afcdacf9385cb07e119415165708
Andrey Melnikov <vafilor@gmail.com>
2020-01-24 12:15:58 -0800

    Removed migration comments as goose dies on them.

--- db/20200123130105_add_latest_workflow_template_version.sql
+++ db/20200123130105_add_latest_workflow_template_version.sql
@@ -1,7 +1,6 @@
 -- +goose Up
 ALTER TABLE workflow_template_versions ADD COLUMN is_latest boolean;
 
--- Set the latest version flag so only the latest versions are the latest.
 UPDATE workflow_template_versions
 SET is_latest = false;
 
@@ -12,7 +11,6 @@ WHERE id IN (
     FROM workflow_template_versions
     GROUP BY workflow_template_id, id
 );
--- end setting latest version
 
 -- +goose Down
 ALTER TABLE workflow_template_versions DROP COLUMN is_latest;
