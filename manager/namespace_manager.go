66a19f1e1b46597b260136a25a3a0dcd4850b361
rushtehrani <r@onepanel.io>
2020-02-02 11:06:43 -0800

    namespace api + server + refactor

--- manager/namespace_manager.go
+++ manager/namespace_manager.go
@@ -1,15 +1,9 @@
 package manager
 
 import (
-	"fmt"
-
 	"github.com/onepanelio/core/model"
 )
 
-var defaultNamespaceLabelKey = labelKeyPrefix + "is-default-namespace"
-
-func (r *ResourceManager) GetDefaultNamespace() (namespaces []*model.Namespace, err error) {
-	return r.kubeClient.ListNamespaces(model.ListOptions{
-		LabelSelector: fmt.Sprintf("%s=%s", defaultNamespaceLabelKey, "true"),
-	})
+func (r *ResourceManager) ListNamespaces(opts model.ListOptions) (namespaces []*model.Namespace, err error) {
+	return r.kubeClient.ListNamespaces(opts)
 }
--- server/server.go
+++ server/server.go
@@ -1,5 +1,22 @@
 package server
 
-import "github.com/onepanelio/core/util"
+import (
+	"github.com/onepanelio/core/api"
+	"github.com/onepanelio/core/model"
+	"github.com/onepanelio/core/util"
+)
 
 var userError *util.UserError
+
+func modelListOptions(lo *api.ListOptions) (listOptions model.ListOptions) {
+	if lo == nil {
+		listOptions = model.ListOptions{}
+		return
+	}
+	listOptions = model.ListOptions{
+		LabelSelector: lo.LabelSelector,
+		FieldSelector: lo.FieldSelector,
+	}
+
+	return
+}
