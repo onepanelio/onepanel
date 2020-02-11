b649f356c8964d89083e103be436417a3298b335
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-02-10 16:07:23 -0800

    Updated workflow_manager.go to use new logging.

--- manager/workflow_manager.go
+++ manager/workflow_manager.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/onepanelio/core/util/logging"
+	log "github.com/sirupsen/logrus"
 	"io"
 	"sort"
 	"strconv"
@@ -25,7 +27,12 @@ var (
 func (r *ResourceManager) CreateWorkflow(namespace string, workflow *model.Workflow) (*model.Workflow, error) {
 	workflowTemplate, err := r.GetWorkflowTemplate(namespace, workflow.WorkflowTemplate.UID, workflow.WorkflowTemplate.Version)
 	if err != nil {
-		return nil, err
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Workflow":  workflow,
+			"Error":     err.Error(),
+		}).Error("Error with getting workflow template.")
+		return nil, util.NewUserError(codes.NotFound, "Error with getting workflow template.")
 	}
 
 	// TODO: Need to pull system parameters from k8s config/secret here, example: HOST
@@ -43,7 +50,12 @@ func (r *ResourceManager) CreateWorkflow(namespace string, workflow *model.Workf
 	(*opts.Labels)[workflowTemplateVersionLabelKey] = fmt.Sprint(workflowTemplate.Version)
 	createdWorkflows, err := r.kubeClient.CreateWorkflow(namespace, workflowTemplate.GetManifestBytes(), opts)
 	if err != nil {
-		return nil, err
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Workflow":  workflow,
+			"Error":     err.Error(),
+		}).Error("Error creating workflow.")
+		return nil, util.NewUserError(codes.Unknown, "Error creating workflow.")
 	}
 
 	workflow.Name = createdWorkflows[0].Name
@@ -59,6 +71,11 @@ func (r *ResourceManager) CreateWorkflow(namespace string, workflow *model.Workf
 func (r *ResourceManager) GetWorkflow(namespace, name string) (workflow *model.Workflow, err error) {
 	wf, err := r.kubeClient.GetWorkflow(namespace, name)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Name":      name,
+			"Error":     err.Error(),
+		}).Error("Workflow not found.")
 		return nil, util.NewUserError(codes.NotFound, "Workflow not found.")
 	}
 
@@ -69,16 +86,31 @@ func (r *ResourceManager) GetWorkflow(namespace, name string) (workflow *model.W
 		32,
 	)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Name":      name,
+			"Error":     err.Error(),
+		}).Error("Invalid version number.")
 		return nil, util.NewUserError(codes.InvalidArgument, "Invalid version number.")
 	}
 	workflowTemplate, err := r.GetWorkflowTemplate(namespace, uid, int32(version))
 	if err != nil {
-		return
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Name":      name,
+			"Error":     err.Error(),
+		}).Error("Cannot get Workflow Template.")
+		return nil, util.NewUserError(codes.NotFound, "Cannot get Workflow Template.")
 	}
 
 	// TODO: Do we need to parse parameters into workflow.Parameters?
 	status, err := json.Marshal(wf.Status)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Name":      name,
+			"Error":     err.Error(),
+		}).Error("Invalid status.")
 		return nil, util.NewUserError(codes.InvalidArgument, "Invalid status.")
 	}
 	workflow = &model.Workflow{
@@ -95,12 +127,22 @@ func (r *ResourceManager) GetWorkflow(namespace, name string) (workflow *model.W
 func (r *ResourceManager) WatchWorkflow(namespace, name string) (<-chan *model.Workflow, error) {
 	wf, err := r.GetWorkflow(namespace, name)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Name":      name,
+			"Error":     err.Error(),
+		}).Error("Workflow template not found.")
 		return nil, util.NewUserError(codes.NotFound, "Workflow template not found.")
 	}
 
 	watcher, err := r.kubeClient.WatchWorkflow(namespace, name)
 	if err != nil {
-		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Name":      name,
+			"Error":     err.Error(),
+		}).Error("Watch Workflow error.")
+		return nil, util.NewUserError(codes.Unknown, "Error with watching workflow.")
 	}
 
 	var workflow *kube.Workflow
@@ -119,6 +161,12 @@ func (r *ResourceManager) WatchWorkflow(namespace, name string) (<-chan *model.W
 			}
 			status, err := json.Marshal(workflow.Status)
 			if err != nil {
+				logging.Logger.Log.WithFields(log.Fields{
+					"Namespace": namespace,
+					"Name":      name,
+					"Workflow":  workflow,
+					"Error":     err.Error(),
+				}).Error("Error with trying to JSON Marshal workflow.Status.")
 				continue
 			}
 			workflowWatcher <- &model.Workflow{
@@ -142,6 +190,13 @@ func (r *ResourceManager) WatchWorkflow(namespace, name string) (<-chan *model.W
 func (r *ResourceManager) GetWorkflowLogs(namespace, name, podName, containerName string) (<-chan *model.LogEntry, error) {
 	wf, err := r.kubeClient.GetWorkflow(namespace, name)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":     namespace,
+			"Name":          name,
+			"PodName":       podName,
+			"ContainerName": containerName,
+			"Error":         err.Error(),
+		}).Error("Workflow not found.")
 		return nil, util.NewUserError(codes.NotFound, "Workflow not found.")
 	}
 
@@ -154,11 +209,25 @@ func (r *ResourceManager) GetWorkflowLogs(namespace, name, podName, containerNam
 	if wf.Status.Nodes[podName].Completed() {
 		config, err = r.getNamespaceConfig(namespace)
 		if err != nil {
+			logging.Logger.Log.WithFields(log.Fields{
+				"Namespace":     namespace,
+				"Name":          name,
+				"PodName":       podName,
+				"ContainerName": containerName,
+				"Error":         err.Error(),
+			}).Error("Can't get configuration.")
 			return nil, util.NewUserError(codes.PermissionDenied, "Can't get configuration.")
 		}
 
 		s3Client, err = r.getS3Client(namespace, config)
 		if err != nil {
+			logging.Logger.Log.WithFields(log.Fields{
+				"Namespace":     namespace,
+				"Name":          name,
+				"PodName":       podName,
+				"ContainerName": containerName,
+				"Error":         err.Error(),
+			}).Error("Can't connect to S3 storage.")
 			return nil, util.NewUserError(codes.PermissionDenied, "Can't connect to S3 storage.")
 		}
 
@@ -167,7 +236,15 @@ func (r *ResourceManager) GetWorkflowLogs(namespace, name, podName, containerNam
 		stream, err = r.kubeClient.GetPodLogs(namespace, podName, containerName)
 	}
 	// TODO: Catch exact kubernetes error
+	//Todo: Can above todo be removed with the logging error?
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":     namespace,
+			"Name":          name,
+			"PodName":       podName,
+			"ContainerName": containerName,
+			"Error":         err.Error(),
+		}).Error("Error with logs.")
 		return nil, util.NewUserError(codes.NotFound, "Log not found.")
 	}
 
@@ -209,6 +286,12 @@ func (r *ResourceManager) ListWorkflows(namespace, workflowTemplateUID, workflow
 	}
 	wfs, err := r.kubeClient.ListWorkflows(namespace, opts)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":               namespace,
+			"WorkflowTemplateUID":     workflowTemplateUID,
+			"WorkflowTemplateVersion": workflowTemplateVersion,
+			"Error":                   err.Error(),
+		}).Error("Workflows not found.")
 		return nil, util.NewUserError(codes.NotFound, "Workflows not found.")
 	}
 	sort.Slice(wfs, func(i, j int) bool {
@@ -231,6 +314,11 @@ func (r *ResourceManager) ListWorkflows(namespace, workflowTemplateUID, workflow
 
 func (r *ResourceManager) TerminateWorkflow(namespace, name string) (err error) {
 	if err = r.kubeClient.TerminateWorkflow(namespace, name); err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Name":      name,
+			"Error":     err.Error(),
+		}).Error("Could not terminate workflow.")
 		return util.NewUserError(codes.Unknown, "Could not terminate workflow.")
 	}
 
@@ -240,7 +328,12 @@ func (r *ResourceManager) TerminateWorkflow(namespace, name string) (err error) 
 func (r *ResourceManager) CreateWorkflowTemplate(namespace string, workflowTemplate *model.WorkflowTemplate) (*model.WorkflowTemplate, error) {
 	allowed, err := r.kubeClient.IsAuthorized(namespace, "create", "argoproj.io", "workflow", "")
 	if err != nil {
-		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("IsAuthorized failed.")
+		return nil, util.NewUserError(codes.Unknown, "Could not create workflow template.")
 	}
 	if !allowed {
 		return nil, util.NewUserError(codes.PermissionDenied, "Permission denied.")
@@ -248,11 +341,21 @@ func (r *ResourceManager) CreateWorkflowTemplate(namespace string, workflowTempl
 
 	// validate workflow template
 	if err := r.kubeClient.ValidateWorkflow(workflowTemplate.GetManifestBytes()); err != nil {
-		return nil, util.NewUserError(codes.InvalidArgument, err.Error())
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Workflow could not be validated.")
+		return nil, util.NewUserError(codes.InvalidArgument, "Workflow Template is invalid.")
 	}
 
 	workflowTemplate, err = r.workflowRepository.CreateWorkflowTemplate(namespace, workflowTemplate)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Could not create workflow template.")
 		return nil, util.NewUserErrorWrap(err, "Workflow template")
 	}
 
@@ -262,7 +365,12 @@ func (r *ResourceManager) CreateWorkflowTemplate(namespace string, workflowTempl
 func (r *ResourceManager) CreateWorkflowTemplateVersion(namespace string, workflowTemplate *model.WorkflowTemplate) (*model.WorkflowTemplate, error) {
 	allowed, err := r.kubeClient.IsAuthorized(namespace, "create", "argoproj.io", "workflow", "")
 	if err != nil {
-		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("IsAuthorized failed.")
+		return nil, util.NewUserError(codes.Unknown, "Could not create template version.")
 	}
 	if !allowed {
 		return nil, util.NewUserError(codes.PermissionDenied, "Permission denied.")
@@ -270,15 +378,30 @@ func (r *ResourceManager) CreateWorkflowTemplateVersion(namespace string, workfl
 
 	// validate workflow template
 	if err := r.kubeClient.ValidateWorkflow(workflowTemplate.GetManifestBytes()); err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Workflow could not be validated.")
 		return nil, util.NewUserError(codes.InvalidArgument, err.Error())
 	}
 
 	if err := r.workflowRepository.RemoveIsLatestFromWorkflowTemplateVersions(workflowTemplate); err != nil {
-		return nil, err
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Could not remove IsLatest from workflow template versions.")
+		return nil, util.NewUserError(codes.Unknown, "Unable to Create Workflow Template Version.")
 	}
 
 	workflowTemplate, err = r.workflowRepository.CreateWorkflowTemplateVersion(namespace, workflowTemplate)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Could not create workflow template version.")
 		return nil, util.NewUserErrorWrap(err, "Workflow template")
 	}
 	if workflowTemplate == nil {
@@ -291,7 +414,12 @@ func (r *ResourceManager) CreateWorkflowTemplateVersion(namespace string, workfl
 func (r *ResourceManager) UpdateWorkflowTemplateVersion(namespace string, workflowTemplate *model.WorkflowTemplate) (*model.WorkflowTemplate, error) {
 	allowed, err := r.kubeClient.IsAuthorized(namespace, "update", "argoproj.io", "workflow", "")
 	if err != nil {
-		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("IsAuthorized failed.")
+		return nil, util.NewUserError(codes.Unknown, "Could not update workflow template version.")
 	}
 	if !allowed {
 		return nil, util.NewUserError(codes.PermissionDenied, "Permission denied.")
@@ -299,17 +427,32 @@ func (r *ResourceManager) UpdateWorkflowTemplateVersion(namespace string, workfl
 
 	// validate workflow template
 	if err := r.kubeClient.ValidateWorkflow(workflowTemplate.GetManifestBytes()); err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Workflow could not be validated.")
 		return nil, util.NewUserError(codes.InvalidArgument, err.Error())
 	}
 
 	originalWorkflowTemplate, err := r.workflowRepository.GetWorkflowTemplate(namespace, workflowTemplate.UID, workflowTemplate.Version)
 	if err != nil {
-		return nil, err
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Could not get workflow template.")
+		return nil, util.NewUserError(codes.Unknown, "Could not update workflow template version.")
 	}
 
 	workflowTemplate.ID = originalWorkflowTemplate.ID
 	workflowTemplate, err = r.workflowRepository.UpdateWorkflowTemplateVersion(workflowTemplate)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Could not update workflow template version.")
 		return nil, util.NewUserErrorWrap(err, "Workflow template")
 	}
 	if workflowTemplate == nil {
@@ -322,6 +465,11 @@ func (r *ResourceManager) UpdateWorkflowTemplateVersion(namespace string, workfl
 func (r *ResourceManager) GetWorkflowTemplate(namespace, uid string, version int32) (workflowTemplate *model.WorkflowTemplate, err error) {
 	allowed, err := r.kubeClient.IsAuthorized(namespace, "get", "argoproj.io", "workflow", "")
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("IsAuthorized failed.")
 		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
 	}
 	if !allowed {
@@ -330,6 +478,11 @@ func (r *ResourceManager) GetWorkflowTemplate(namespace, uid string, version int
 
 	workflowTemplate, err = r.workflowRepository.GetWorkflowTemplate(namespace, uid, version)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace":        namespace,
+			"WorkflowTemplate": workflowTemplate,
+			"Error":            err.Error(),
+		}).Error("Get Workflow Template failed.")
 		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
 	}
 	if workflowTemplate == nil {
@@ -342,6 +495,11 @@ func (r *ResourceManager) GetWorkflowTemplate(namespace, uid string, version int
 func (r *ResourceManager) ListWorkflowTemplateVersions(namespace, uid string) (workflowTemplateVersions []*model.WorkflowTemplate, err error) {
 	allowed, err := r.kubeClient.IsAuthorized(namespace, "list", "argoproj.io", "workflow", "")
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"UID":       uid,
+			"Error":     err.Error(),
+		}).Error("IsAuthorized failed.")
 		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
 	}
 	if !allowed {
@@ -350,6 +508,11 @@ func (r *ResourceManager) ListWorkflowTemplateVersions(namespace, uid string) (w
 
 	workflowTemplateVersions, err = r.workflowRepository.ListWorkflowTemplateVersions(namespace, uid)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"UID":       uid,
+			"Error":     err.Error(),
+		}).Error("Workflow template versions not found.")
 		return nil, util.NewUserError(codes.NotFound, "Workflow template versions not found.")
 	}
 
@@ -359,7 +522,11 @@ func (r *ResourceManager) ListWorkflowTemplateVersions(namespace, uid string) (w
 func (r *ResourceManager) ListWorkflowTemplates(namespace string) (workflowTemplateVersions []*model.WorkflowTemplate, err error) {
 	allowed, err := r.kubeClient.IsAuthorized(namespace, "list", "argoproj.io", "workflow", "")
 	if err != nil {
-		return nil, util.NewUserError(codes.Unknown, "Unknown error.")
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Error":     err.Error(),
+		}).Error("IsAuthorized failed.")
+		return nil, util.NewUserError(codes.Unknown, "Unable to list workflow templates.")
 	}
 	if !allowed {
 		return nil, util.NewUserError(codes.PermissionDenied, "Permission denied.")
@@ -367,6 +534,10 @@ func (r *ResourceManager) ListWorkflowTemplates(namespace string) (workflowTempl
 
 	workflowTemplateVersions, err = r.workflowRepository.ListWorkflowTemplates(namespace)
 	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"Error":     err.Error(),
+		}).Error("Workflow templates not found.")
 		return nil, util.NewUserError(codes.NotFound, "Workflow templates not found.")
 	}
 
@@ -376,7 +547,12 @@ func (r *ResourceManager) ListWorkflowTemplates(namespace string) (workflowTempl
 func (r *ResourceManager) ArchiveWorkflowTemplate(namespace, uid string) (archived bool, err error) {
 	allowed, err := r.kubeClient.IsAuthorized(namespace, "delete", "argoproj.io", "workflow", "")
 	if err != nil {
-		return false, util.NewUserError(codes.Unknown, "Unknown error.")
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"UID":       uid,
+			"Error":     err.Error(),
+		}).Error("IsAuthorized failed.")
+		return false, util.NewUserError(codes.Unknown, "Unable to archive workflow template.")
 	}
 	if !allowed {
 		return false, util.NewUserError(codes.PermissionDenied, "Permission denied.")
@@ -384,7 +560,12 @@ func (r *ResourceManager) ArchiveWorkflowTemplate(namespace, uid string) (archiv
 
 	workflowTemplate, err := r.workflowRepository.GetWorkflowTemplate(namespace, uid, 0)
 	if err != nil {
-		return false, util.NewUserError(codes.Unknown, "Unknown error.")
+		logging.Logger.Log.WithFields(log.Fields{
+			"Namespace": namespace,
+			"UID":       uid,
+			"Error":     err.Error(),
+		}).Error("Get Workflow Template failed.")
+		return false, util.NewUserError(codes.Unknown, "Unable to archive workflow template.")
 	}
 	if workflowTemplate == nil {
 		return false, util.NewUserError(codes.NotFound, "Workflow template not found.")
@@ -392,7 +573,14 @@ func (r *ResourceManager) ArchiveWorkflowTemplate(namespace, uid string) (archiv
 
 	archived, err = r.workflowRepository.ArchiveWorkflowTemplate(namespace, uid)
 	if !archived || err != nil {
-		return false, util.NewUserError(codes.Unknown, "Unknown error.")
+		if err != nil {
+			logging.Logger.Log.WithFields(log.Fields{
+				"Namespace": namespace,
+				"UID":       uid,
+				"Error":     err.Error(),
+			}).Error("Archive Workflow Template failed.")
+		}
+		return false, util.NewUserError(codes.Unknown, "Unable to archive workflow template.")
 	}
 
 	return
