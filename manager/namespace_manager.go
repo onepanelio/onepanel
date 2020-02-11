1e7f034384cb176de59a31315c59e39b68aff1c8
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-02-10 16:15:07 -0800

    Updated namespace_manager.go to use new logging.

--- manager/namespace_manager.go
+++ manager/namespace_manager.go
@@ -2,6 +2,8 @@ package manager
 
 import (
 	"fmt"
+	"github.com/onepanelio/core/util/logging"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/onepanelio/core/model"
 )
@@ -9,7 +11,13 @@ import (
 var onepanelEnabledLabelKey = labelKeyPrefix + "enabled"
 
 func (r *ResourceManager) ListNamespaces() (namespaces []*model.Namespace, err error) {
-	return r.kubeClient.ListNamespaces(model.ListOptions{
+	namespaces, err = r.kubeClient.ListNamespaces(model.ListOptions{
 		LabelSelector: fmt.Sprintf("%s=%s", onepanelEnabledLabelKey, "true"),
 	})
+	if err != nil {
+		logging.Logger.Log.WithFields(log.Fields{
+			"Error": err.Error(),
+		}).Error("ListNamespaces failed.")
+	}
+	return
 }
