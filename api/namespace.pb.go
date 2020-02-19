d3c13fbb0d8f740ece16224d41dd74370a49b7a8
Andrey Melnikov <vafilor@gmail.com>
2020-02-19 11:11:45 -0800

    Updated generated swagger docs

--- api/namespace.pb.go
+++ api/namespace.pb.go
@@ -140,11 +140,11 @@ var fileDescriptor_ecb1e126f615f5dd = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // NamespaceServiceClient is the client API for NamespaceService service.
 //
@@ -154,10 +154,10 @@ type NamespaceServiceClient interface {
 }
 
 type namespaceServiceClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewNamespaceServiceClient(cc grpc.ClientConnInterface) NamespaceServiceClient {
+func NewNamespaceServiceClient(cc *grpc.ClientConn) NamespaceServiceClient {
 	return &namespaceServiceClient{cc}
 }
 
