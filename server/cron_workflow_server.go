4d66356c26a4bd85bc4efbb5fb4ced915c7dc036
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-03-25 11:42:09 -0700

    Fixing double error check.

--- server/cron_workflow_server.go
+++ server/cron_workflow_server.go
@@ -54,9 +54,7 @@ func (c *CronWorkflowServer) CreateCronWorkflow(ctx context.Context, req *api.Cr
 
 	cwf, err := client.CreateCronWorkflow(req.Namespace, cronWorkflow)
 	if err != nil {
-		if err != nil {
-			return nil, err
-		}
+		return nil, err
 	}
 	return apiCronWorkflow(cwf), nil
 }
