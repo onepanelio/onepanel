8719a1ee2af65e604bd89ca0210ba6bbb8cce197
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-03-25 11:08:19 -0700

    Adding apiCronWorkflow function, to return the expected API response struct.

--- server/cron_workflow_server.go
+++ server/cron_workflow_server.go
@@ -13,4 +13,17 @@ func NewCronWorkflowServer() *CronWorkflowServer {
 
 func (c CronWorkflowServer) CreateCronWorkflow(ctx context.Context, req *api.CreateWorkflowRequest) (*api.CronWorkflow, error) {
 	panic("implement me")
+func apiCronWorkflow(cwf *v1.CronWorkflow) (cronWorkflow *api.CronWorkflow) {
+	cronWorkflow = &api.CronWorkflow{
+		Schedule:                   cwf.Schedule,
+		Timezone:                   cwf.Timezone,
+		Suspend:                    cwf.Suspend,
+		ConcurrencyPolicy:          cwf.ConcurrencyPolicy,
+		StartingDeadlineSeconds:    cwf.StartingDeadlineSeconds,
+		SuccessfulJobsHistoryLimit: cwf.SuccessfulJobsHistoryLimit,
+		FailedJobsHistoryLimit:     cwf.FailedJobsHistoryLimit,
+		WorkflowExecution:          GenApiWorkflowExecution(cwf.WorkflowExecution),
+	}
+	return
+}
 }
