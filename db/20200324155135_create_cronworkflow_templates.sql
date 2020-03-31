f21072f4ec0ddfb52910fa8213b10f2e819ccb95
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-03-31 15:31:17 -0700

    Adjusting Create CronWorkflow Templates SQL. - Adding columns to support tracking CronWorkflows to WorkflowTemplates - Dropping un-needed table

--- db/20200324155135_create_cronworkflow_templates.sql
+++ db/20200324155135_create_cronworkflow_templates.sql
@@ -1,19 +1,18 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE TABLE cron_workflow_templates
+CREATE TABLE cron_workflows_templates
 (
     id          serial PRIMARY KEY,
     uid         varchar(36) UNIQUE NOT NULL CHECK(uid <> ''),
+    workflow_template_id integer,
     name        text NOT NULL CHECK(name <> ''),
     namespace   varchar(36) NOT NULL,
-    is_archived boolean DEFAULT false,
 
     -- auditing info
     created_at  timestamp NOT NULL DEFAULT (NOW() at time zone 'utc'),
-    modified_at timestamp,
+    modified_at timestamp
 
-    UNIQUE (name, namespace)
 );
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-DROP TABLE cron_workflow_templates;
+DROP TABLE cron_workflows_templates;
