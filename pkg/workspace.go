3bcdb7681a8c493af56feb36174b6e5b328be8cd
rushtehrani <r@onepanel.io>
2020-04-20 19:22:35 -0700

    use []byte and additional invalid tests

--- pkg/workspace.go
+++ pkg/workspace.go
@@ -9,44 +9,44 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-func parseServicePorts(template string) (servicePorts []*corev1.ServicePort, err error) {
-	if err = yaml.UnmarshalStrict([]byte(template), &servicePorts); err != nil {
+func parseServicePorts(template []byte) (servicePorts []*corev1.ServicePort, err error) {
+	if err = yaml.UnmarshalStrict(template, &servicePorts); err != nil {
 		return
 	}
 
 	return
 }
 
-func parseHTTPRoutes(template string) (HTTPRoutes []*networking.HTTPRoute, err error) {
-	if err = yaml.UnmarshalStrict([]byte(template), &HTTPRoutes); err != nil {
+func parseHTTPRoutes(template []byte) (HTTPRoutes []*networking.HTTPRoute, err error) {
+	if err = yaml.UnmarshalStrict(template, &HTTPRoutes); err != nil {
 		return
 	}
 
 	return
 }
 
-func parseVolumeClaims(template string) (persistentVolumeClaims []*corev1.PersistentVolumeClaim, err error) {
-	if err = yaml.UnmarshalStrict([]byte(template), &persistentVolumeClaims); err != nil {
+func parseVolumeClaims(template []byte) (persistentVolumeClaims []*corev1.PersistentVolumeClaim, err error) {
+	if err = yaml.UnmarshalStrict(template, &persistentVolumeClaims); err != nil {
 		return
 	}
 
 	return
 }
 
-func parseContainers(template string) (containers []*corev1.Container, err error) {
-	if err = yaml.UnmarshalStrict([]byte(template), &containers); err != nil {
+func parseContainers(template []byte) (containers []*corev1.Container, err error) {
+	if err = yaml.UnmarshalStrict(template, &containers); err != nil {
 		return
 	}
 
 	return
 }
 
-func (c *Client) CreateWorkspace(namespace, parametersTemplate, containersTemplate, portsTemplate, routesTemplate, volumeClaimsTemplate string) (err error) {
+func (c *Client) CreateWorkspace(namespace string, parametersTemplate, containersTemplate, portsTemplate, routesTemplate, volumeClaimsTemplate []byte) (err error) {
 	_, err = parseContainers(containersTemplate)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"Namespace": namespace,
-			"Error":     err.Error(),
+			"Error":     err.Error,
 		}).Error("Invalid Workspace Containers template.")
 		return util.NewUserError(codes.InvalidArgument, err.Error())
 	}
