2e18d7005a1ce26f190358d8d7f0070c2774055f
rushtehrani <r@onepanel.io>
2020-04-23 14:25:32 -0700

    update tests

--- pkg/client_test.go
+++ pkg/client_test.go
@@ -1,8 +1,30 @@
 package v1
 
 import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
+)
+
+var (
+	fakeSystemSecret = &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "onepanel",
+			Namespace: "onepanel",
+		},
+	}
+
+	fakeSystemConfigMap = &v1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "onepanel",
+			Namespace: "onepanel",
+		},
+		Data: map[string]string{
+			"ONEPANEL_HOST":            "demo.onepanel.site",
+			"applicationNodePoolLabel": "beta.kubernetes.io/instance-type",
+		},
+	}
 )
 
 func NewTestClient(objects ...runtime.Object) (client *Client) {
