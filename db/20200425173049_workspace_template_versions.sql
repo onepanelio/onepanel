151bf47dba1425042b3d1dc2bbe9271f7ee5fb9c
rushtehrani <r@onepanel.io>
2020-04-26 11:27:18 -0700

    workspace database table updates

--- db/20200425173049_workspace_template_versions.sql
+++ db/20200425173049_workspace_template_versions.sql
@@ -1,15 +1,15 @@
 -- +goose Up
 CREATE TABLE workspace_template_versions
 (
-    id                      serial PRIMARY KEY,
-    workspace_template_id   integer NOT NULL REFERENCES workspace_templates ON DELETE CASCADE,
-    version                 integer NOT NULL,
-    manifest                text NOT NULL,
-    is_latest               boolean DEFAULT false,
+    id                              serial PRIMARY KEY,
+    workspace_template_id           integer NOT NULL REFERENCES workspace_templates ON DELETE CASCADE,
+    version                         integer NOT NULL,
+    manifest                        text NOT NULL,
+    is_latest                       boolean DEFAULT false,
 
     -- auditing info
-    created_at              timestamp NOT NULL DEFAULT (NOW() at time zone 'utc'),
-    modified_at             timestamp
+    created_at                      timestamp NOT NULL DEFAULT (NOW() at time zone 'utc'),
+    modified_at                     timestamp
 );
 
 -- +goose Down
