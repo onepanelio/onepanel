872f6c97dcbec3255509ec16a0305a79ec77bf95
Andrey Melnikov <vafilor@gmail.com>
2020-04-27 15:49:15 -0700

    fix: migration for uids

---
+++ db/20200427114910_add_uid_to_workflow_template_versions_and_labels.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+ALTER TABLE workflow_template_versions ADD COLUMN uid varchar(36) UNIQUE CHECK(uid <> '');
+UPDATE workflow_template_versions SET uid = uuid_generate_v4();
+ALTER TABLE workflow_template_versions ALTER COLUMN uid SET NOT NULL;
+
+ALTER TABLE labels ADD COLUMN uid varchar(36) UNIQUE CHECK(uid <> '');
+UPDATE labels SET uid = uuid_generate_v4();
+ALTER TABLE labels ALTER COLUMN uid SET NOT NULL;
+
+-- +goose Down
+ALTER TABLE workflow_template_versions DROP COLUMN uid;
+ALTER TABLE labels DROP COLUMN uid;
