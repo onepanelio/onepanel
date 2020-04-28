d8bf13871ab8bee5de752d9a79480372eba4c38b
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-04-28 12:05:31 -0700

    Adding extension enabling to handle this error: ``` FATA[0000] Failed to run database migrations: failed to run SQL migration "20200427114910_add_uid_to_workflow_template_versions_and_labels.sql": failed to execute SQL query "UPDATE workflow_template_versions SET uid = uuid_generate_v4();\n": pq: function uuid_generate_v4() does not exist ```

--- db/20200427114910_add_uid_to_workflow_template_versions_and_labels.sql
+++ db/20200427114910_add_uid_to_workflow_template_versions_and_labels.sql
@@ -1,4 +1,5 @@
 -- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 ALTER TABLE workflow_template_versions ADD COLUMN uid varchar(36) UNIQUE CHECK(uid <> '');
 UPDATE workflow_template_versions SET uid = uuid_generate_v4();
 ALTER TABLE workflow_template_versions ALTER COLUMN uid SET NOT NULL;
@@ -10,3 +11,4 @@ ALTER TABLE labels ALTER COLUMN uid SET NOT NULL;
 -- +goose Down
 ALTER TABLE workflow_template_versions DROP COLUMN uid;
 ALTER TABLE labels DROP COLUMN uid;
+DROP EXTENSION IF EXISTS "uuid-ossp"
