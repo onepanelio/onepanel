015d78f1a35cb8a9b72979d582dc57a711f3e6d0
rushtehrani <r@onepanel.io>
2020-04-28 13:59:30 -0700

    support hint and displayName

--- pkg/apis/core/v1/common_types.go
+++ pkg/apis/core/v1/common_types.go
@@ -9,11 +9,13 @@ type ParameterOption struct {
 }
 
 type Parameter struct {
-	Name     string             `json:"name" protobuf:"bytes,1,opt,name=name"`
-	Value    *string            `json:"value,omitempty" protobuf:"bytes,2,opt,name=value"`
-	Type     string             `json:"type" protobuf:"bytes,3,opt,name=type"`
-	Options  []*ParameterOption `json:"options,omitempty" protobuf:"bytes,4,opt,name=options"`
-	Required bool               `json:"required,omitempty" protobuf:"bytes,5,opt,name=required"`
+	Name        string             `json:"name" protobuf:"bytes,1,opt,name=name"`
+	Value       *string            `json:"value,omitempty" protobuf:"bytes,2,opt,name=value"`
+	Type        string             `json:"type" protobuf:"bytes,3,opt,name=type"`
+	DisplayName *string            `json:"displayName" protobuf:"bytes,4,opt,name=displayName"`
+	Hint        *string            `json:"hint" protobuf:"bytes,5,opt,name=hint"`
+	Options     []*ParameterOption `json:"options,omitempty" protobuf:"bytes,6,opt,name=options"`
+	Required    bool               `json:"required,omitempty" protobuf:"bytes,7,opt,name=required"`
 }
 
 type Arguments struct {
