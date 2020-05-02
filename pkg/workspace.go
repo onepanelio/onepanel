6adccf5174fb3d604d40d6613d9c2efdddd13b2c
rushtehrani <r@onepanel.io>
2020-05-01 18:06:03 -0700

    refactor function

--- pkg/workspace.go
+++ pkg/workspace.go
@@ -7,40 +7,34 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
-func injectWorkspaceParameterValues(parameters []Parameter, workspaceAction, resourceAction string) (workspace *Workspace, err error) {
-	workspace = &Workspace{}
-	for _, p := range parameters {
-		// TODO: This can be removed when we have validation on param level
+func injectWorkspaceParameterValues(workspace *Workspace, workspaceAction, resourceAction string) (err error) {
+	for _, p := range workspace.Parameters {
 		if p.Name == "sys-name" {
+			// TODO: These if statements can be removed when we have validation on param level
 			if p.Value == nil {
-				return nil, util.NewUserError(codes.InvalidArgument, "Workspace name is required.")
+				return util.NewUserError(codes.InvalidArgument, "Workspace name is required.")
 			}
 			if !validate.IsDNSHost(*p.Value) {
-				return nil, util.NewUserError(codes.InvalidArgument, "Workspace name is not valid.")
+				return util.NewUserError(codes.InvalidArgument, "Workspace name is not valid.")
 			}
 			workspace.Name = *p.Value
 		}
 	}
-	parameters = append(parameters, Parameter{
+	workspace.Parameters = append(workspace.Parameters, Parameter{
 		Name:  "sys-workspace-action",
 		Value: ptr.String(workspaceAction),
 	}, Parameter{
 		Name:  "sys-resource-action",
 		Value: ptr.String(resourceAction),
 	})
-	workspace.Parameters = parameters
 
 	return
 }
 
 func (c *Client) CreateWorkspace(namespace string, workspace *Workspace) (*Workspace, error) {
-	ws, err := injectWorkspaceParameterValues(workspace.Parameters, "create", "apply")
-	if err != nil {
+	if err := injectWorkspaceParameterValues(workspace, "create", "apply"); err != nil {
 		return nil, err
 	}
-
-	workspace.Name = ws.Name
-	workspace.Parameters = ws.Parameters
 
 	workflowTemplate, err := c.getWorkspaceTemplateWorkflowTemplate(namespace,
 		workspace.WorkspaceTemplate.UID, workspace.WorkspaceTemplate.Version)
