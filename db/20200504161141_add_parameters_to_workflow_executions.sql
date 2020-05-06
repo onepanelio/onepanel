485c75695d57bc7ade0c93c244aa1abeb3af5489
Andrey Melnikov <vafilor@gmail.com>
2020-05-06 11:36:10 -0700

    fix: changed default workflow execution parameters to be an array.

--- db/20200504161141_add_parameters_to_workflow_executions.sql
+++ db/20200504161141_add_parameters_to_workflow_executions.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 ALTER TABLE workflow_executions ADD COLUMN parameters JSONB;
-UPDATE workflow_executions SET parameters = '{}'::JSONB;
+UPDATE workflow_executions SET parameters = '[]'::JSONB;
 ALTER TABLE workflow_executions ALTER COLUMN parameters SET NOT NULL;
 
 -- +goose Down
