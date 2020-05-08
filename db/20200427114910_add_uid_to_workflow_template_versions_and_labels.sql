a04bf23074435a6795534305d2811415954fc523
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-05-08 15:35:52 -0700

    Changing uid varchar length.

--- db/20200427114910_add_uid_to_workflow_template_versions_and_labels.sql
+++ db/20200427114910_add_uid_to_workflow_template_versions_and_labels.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-ALTER TABLE workflow_template_versions ADD COLUMN uid varchar(36) UNIQUE CHECK(uid <> '');
+ALTER TABLE workflow_template_versions ADD COLUMN uid varchar(63) UNIQUE CHECK(uid <> '');
 UPDATE workflow_template_versions SET uid = uuid_generate_v4();
 ALTER TABLE workflow_template_versions ALTER COLUMN uid SET NOT NULL;
 
-ALTER TABLE labels ADD COLUMN uid varchar(36) UNIQUE CHECK(uid <> '');
+ALTER TABLE labels ADD COLUMN uid varchar(63) UNIQUE CHECK(uid <> '');
 UPDATE labels SET uid = uuid_generate_v4();
 ALTER TABLE labels ALTER COLUMN uid SET NOT NULL;
 
