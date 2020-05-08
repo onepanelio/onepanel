4a7d481ff6aea43eb3d762ddbcb7d2d11d74960a
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-05-08 16:11:22 -0700

    Changing length check.

--- pkg/util/uid/uid.go
+++ pkg/util/uid/uid.go
@@ -9,7 +9,7 @@ import (
 func GenerateK8sValidName(input string) (string, error) {
 	re, _ := regexp.Compile(`[^a-zA-Z0-9-]{1,}`)
 	cleanUp := strings.ToLower(re.ReplaceAllString(input, `-`))
-	if len(cleanUp) > 63 {
+	if len(cleanUp) > 30 {
 		return "", errors.New("Length of string exceeds 63, invalid for k8s.")
 	}
 	return strings.ToLower(re.ReplaceAllString(input, `-`)), nil
