3a8e170e737ee30b82dd86b7e52b7e73c7550bfc
Rush Tehrani <r@onepanel.io>
2020-05-08 13:11:13 -0700

    Revert "update: simplified request for creating a workflow execution."

--- api/workspace.pb.gw.go
+++ api/workspace.pb.gw.go
@@ -39,7 +39,7 @@ func request_WorkspaceService_CreateWorkspace_0(ctx context.Context, marshaler r
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Workspace); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -74,7 +74,7 @@ func local_request_WorkspaceService_CreateWorkspace_0(ctx context.Context, marsh
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Workspace); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
