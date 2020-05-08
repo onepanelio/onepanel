55db5f2708934b3d336de2b0c291cacbefa86f59
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-05-08 14:49:47 -0700

    Adding util function to generate k8s compatible uids (to be used for k8s name).

---
+++ pkg/util/uid/uid.go
@@ -0,0 +1,16 @@
+package uid
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+func GenerateK8sValidName(input string) (string, error) {
+	re, _ := regexp.Compile(`[^a-zA-Z0-9-]{1,}`)
+	cleanUp := strings.ToLower(re.ReplaceAllString(input, `-`))
+	if len(cleanUp) > 63 {
+		return "", errors.New("Length of string exceeds 63, invalid for k8s.")
+	}
+	return strings.ToLower(re.ReplaceAllString(input, `-`)), nil
+}
