c7bd0177252f87f2412636d265ad63452397fd2e
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-05-11 12:01:37 -0700

    Fixing error information.

--- pkg/util/uid/uid.go
+++ pkg/util/uid/uid.go
@@ -10,7 +10,7 @@ func GenerateK8sValidName(input string) (string, error) {
 	re, _ := regexp.Compile(`[^a-zA-Z0-9-]{1,}`)
 	cleanUp := strings.ToLower(re.ReplaceAllString(input, `-`))
 	if len(cleanUp) > 30 {
-		return "", errors.New("Length of string exceeds 63, invalid for k8s.")
+		return "", errors.New("Length of string exceeds 30, invalid for k8s.")
 	}
 	return strings.ToLower(re.ReplaceAllString(input, `-`)), nil
 }
