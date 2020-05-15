fb09fb621d3b40c2bdda7101c138cbc8d9552956
rushtehrani <r@onepanel.io>
2020-05-15 13:11:04 -0700

    fix migrations

--- db/20200413141037_create_workflow_execution.sql
+++ db/20200413141037_create_workflow_execution.sql
@@ -3,6 +3,7 @@
 CREATE TABLE workflow_executions
 (
     id                   serial PRIMARY KEY,
+    uid                  varchar(30) UNIQUE NOT NULL CHECK(uid <> ''),
     workflow_template_id integer     NOT NULL REFERENCES workflow_templates ON DELETE CASCADE,
     name                 text        NOT NULL CHECK (name <> ''),
     namespace            varchar(30) NOT NULL,
