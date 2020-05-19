0aced56569743ffe0b2f6559069318ef288e05c1
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-05-19 15:13:27 -0700

    Added checking for existence of constraints. - Since this is run on start-up and not in production, we're fine

--- db/20200518163745_AddConditionalUniqueIndexToWorkflowTemplates.sql
+++ db/20200518163745_AddConditionalUniqueIndexToWorkflowTemplates.sql
@@ -1,6 +1,6 @@
 -- +goose Up
-ALTER TABLE workflow_templates DROP CONSTRAINT workflow_templates_uid_key;
-ALTER TABLE workflow_templates DROP CONSTRAINT workflow_templates_uid_namespace_key;
+ALTER TABLE workflow_templates DROP CONSTRAINT IF EXISTS workflow_templates_uid_key;
+ALTER TABLE workflow_templates DROP CONSTRAINT IF EXISTS workflow_templates_uid_namespace_key;
 CREATE UNIQUE INDEX workflow_templates_name_namespace_key ON workflow_templates (name, namespace) WHERE is_archived = false;
 CREATE UNIQUE INDEX workflow_templates_uid_namespace_key ON workflow_templates (uid, namespace) WHERE is_archived = false;
 
