ea5d4d56fbb4b51a3c93e0222b9d600d1520e12a
Andrey Melnikov <vafilor@gmail.com>
2020-05-21 15:13:26 -0700

    update: Updated GetWorkflowExecution request to also get information for the workflow template, since we query for it anyway.

--- pkg/workflow_execution.go
+++ pkg/workflow_execution.go
@@ -551,6 +551,8 @@ func (c *Client) GetWorkflowExecution(namespace, uid string) (workflow *Workflow
 	workflow = &WorkflowExecution{}
 
 	query, args, err := sb.Select(getWorkflowExecutionColumns("we", "")...).
+		Columns(getWorkflowTemplateColumns("wt", "workflow_template")...).
+		Columns(`wtv.manifest "workflow_template.manifest"`).
 		From("workflow_executions we").
 		Join("workflow_template_versions wtv ON wtv.id = we.workflow_template_version_id").
 		Join("workflow_templates wt ON wt.id = wtv.workflow_template_id").
@@ -600,7 +602,6 @@ func (c *Client) GetWorkflowExecution(namespace, uid string) (workflow *Workflow
 		return nil, util.NewUserError(codes.NotFound, "Cannot get Workflow Template.")
 	}
 
-	// TODO: Do we need to parse parameters into workflow.Parameters?
 	manifest, err := json.Marshal(wf)
 	if err != nil {
 		log.WithFields(log.Fields{
