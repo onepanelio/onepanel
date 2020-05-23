7ff3ad0c98cf4cdce5e63d34f8e94a233b8288fb
rushtehrani <r@onepanel.io>
2020-05-22 17:32:19 -0700

    uid => name in down migration

--- db/20200522164526_alter_workflow_executions_column_sizes.sql
+++ db/20200522164526_alter_workflow_executions_column_sizes.sql
@@ -4,4 +4,4 @@ ALTER TABLE workflow_executions ALTER COLUMN name TYPE varchar(63);
 
 -- +goose Down
 ALTER TABLE workflow_executions ALTER COLUMN uid TYPE varchar(30);
-ALTER TABLE workflow_executions ALTER COLUMN uid TYPE text;
+ALTER TABLE workflow_executions ALTER COLUMN name TYPE text;
