bf37713371210f27620c7a461c2257ff0cba259d
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-06-02 16:18:37 -0700

    Migration fails silently because the insert statement lacked "uid" value.

--- pkg/workspace_template.go
+++ pkg/workspace_template.go
@@ -475,6 +475,7 @@ func (c *Client) createWorkspaceTemplate(namespace string, workspaceTemplate *Wo
 	workspaceTemplateVersionID := uint64(0)
 	err = sb.Insert("workspace_template_versions").
 		SetMap(sq.Eq{
+			"uid":                   uid,
 			"version":               workspaceTemplate.Version,
 			"is_latest":             workspaceTemplate.IsLatest,
 			"manifest":              workspaceTemplate.Manifest,
