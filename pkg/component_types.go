6753ead9f806171818ef86d06dad2c00b83889ef
Andrey Melnikov <vafilor@gmail.com>
2020-07-23 20:10:35 -0700

    doc: component type

--- pkg/component_types.go
+++ pkg/component_types.go
@@ -1,5 +1,7 @@
 package v1
 
+// Component represents an installable "component" added to the system.
+// This can be something like modeldb, or some other service that complements the main system.
 type Component struct {
 	Name string
 	URL  string
