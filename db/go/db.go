ac5a394366987dbc693bf252c31960d6d2426e82
Andrey Melnikov <vafilor@gmail.com>
2020-07-28 20:52:02 -0700

    update: added utility to go migrations to find out if it has been run before as a sql migration

--- db/go/db.go
+++ db/go/db.go
@@ -4,20 +4,45 @@ import (
 	sq "github.com/Masterminds/squirrel"
 	"github.com/jmoiron/sqlx"
 	v1 "github.com/onepanelio/core/pkg"
+	"log"
 )
 
 // initializedMigrations is used to keep track of which migrations have been initialized.
-// if they are initialzed more than once, goose panics.
+// if they are initialized more than once, goose panics.
 var initializedMigrations = make(map[int]bool)
+
+// sqlRanMigrations keeps track of all the sql migrations that have been run.
+// we need to know this because in an older version some go migrations ran alongside sql.
+// So if they have already been run, we can't run them again.
+var sqlRanMigrations = make(map[uint64]bool)
+
+// migrationHasAlreadyBeenRun returns true if the migration has already been run in sql
+// see sqlRanMigrations var
+func migrationHasAlreadyBeenRun(version int) bool {
+	_, ok := sqlRanMigrations[uint64(version)]
+	return ok
+}
 
 // Initialize sets up the go migrations.
 func Initialize() {
+	client, err := getClient()
+	if err != nil {
+		log.Fatalf("unable to get client for go migrations: %v", err)
+	}
+
+	migrationsRan, err := getRanSQLMigrations(client)
+	if err != nil {
+		log.Fatalf("Unable to get already run sql migrations: %v", err)
+	}
+	sqlRanMigrations = migrationsRan
+
 	initialize20200525160514()
 	initialize20200528140124()
 	initialize20200605090509()
 	initialize20200605090535()
 	initialize20200626113635()
 	initialize20200704151301()
+	initialize20200728190804()
 }
 
 func getClient() (*v1.Client, error) {
