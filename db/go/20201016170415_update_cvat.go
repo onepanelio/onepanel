2cc4cbd6f8917c32ae210edf9495ee4ef0810521
Andrey Melnikov <vafilor@gmail.com>
2020-10-16 17:33:35 -0700

    cleanup: simplified updating workspace template into a separate function

--- db/go/20201016170415_update_cvat.go
+++ db/go/20201016170415_update_cvat.go
@@ -2,8 +2,6 @@ package migration
 
 import (
 	"database/sql"
-	v1 "github.com/onepanelio/core/pkg"
-	uid2 "github.com/onepanelio/core/pkg/util/uid"
 	"github.com/pressly/goose"
 )
 
@@ -17,43 +15,7 @@ func initialize20201016170415() {
 // Up20201016170415 updates cvat to a new version
 func Up20201016170415(tx *sql.Tx) error {
 	// This code is executed when the migration is applied.
-	client, err := getClient()
-	if err != nil {
-		return err
-	}
-	defer client.DB.Close()
-
-	namespaces, err := client.ListOnepanelEnabledNamespaces()
-	if err != nil {
-		return err
-	}
-
-	newManifest, err := readDataFile("cvat9.yaml")
-	if err != nil {
-		return err
-	}
-
-	uid, err := uid2.GenerateUID(cvatTemplateName, 30)
-	if err != nil {
-		return err
-	}
-
-	for _, namespace := range namespaces {
-		workspaceTemplate := &v1.WorkspaceTemplate{
-			UID:      uid,
-			Name:     cvatTemplateName,
-			Manifest: newManifest,
-		}
-		err = ReplaceArtifactRepositoryType(client, namespace, nil, workspaceTemplate)
-		if err != nil {
-			return err
-		}
-		if _, err := client.UpdateWorkspaceTemplateManifest(namespace.Name, uid, workspaceTemplate.Manifest); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return updateWorkspaceTemplateManifest("cvat9.yaml", cvatTemplateName)
 }
 
 // Down20201016170415 does nothing
