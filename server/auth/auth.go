50145cccdf2f7c7cfc71cf28c6678d1d27a9a1f3
Andrey Melnikov <vafilor@gmail.com>
2020-10-23 11:40:29 -0700

    feat: added more logging for getBearerToken errors

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"github.com/onepanelio/core/api"
 	"github.com/onepanelio/core/pkg/util"
+	log "github.com/sirupsen/logrus"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
 	"strings"
@@ -31,6 +32,9 @@ const (
 func getBearerToken(ctx context.Context) (*string, bool) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
+		log.WithFields(log.Fields{
+			"Method": "getBearerToken",
+		}).Error("Unable to get metadata from incoming context")
 		return nil, false
 	}
 
@@ -62,10 +66,21 @@ func getBearerToken(ctx context.Context) (*string, bool) {
 		return &t, true
 	}
 
+	log.WithFields(log.Fields{
+		"Method": "getBearerToken",
+	}).Error("Unable to get BearerToken:", md)
+
 	return nil, false
 }
 
 func getClient(ctx context.Context, kubeConfig *v1.Config, db *v1.DB, sysConfig v1.SystemConfig) (context.Context, error) {
+	if kubeConfig == nil {
+		return nil, fmt.Errorf("getClient - nil passed in for kubeConfig")
+	}
+	if db == nil {
+		return nil, fmt.Errorf("getClient - nil passed in for db")
+	}
+
 	bearerToken, ok := getBearerToken(ctx)
 	if !ok {
 		return nil, status.Error(codes.Unauthenticated, `Missing or invalid "authorization" header.`)
