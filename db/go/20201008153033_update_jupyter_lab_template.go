6591d19daec3ddec4d9df037d148142fb2bd01e1
Aleksandr Melnikov <aleksandr.melnikov@fmailbox.com>
2020-10-28 12:51:10 -0700

    Adding --minimize=False to build command. - This is a trade-off to reduce workspace start-up time, but increase browser load times for Jupyterlab.

--- db/go/20201008153033_update_jupyter_lab_template.go
+++ db/go/20201008153033_update_jupyter_lab_template.go
@@ -33,7 +33,7 @@ containers:
           condayml="/data/.environment.yml";
           jupytertxt="/data/.jupexported.txt";
           if [ -f "$condayml" ]; then conda env update -f $condayml; fi;
-          if [ -f "$jupytertxt" ]; then cat $jupytertxt | xargs -n 1 jupyter labextension install --no-build && jupyter lab build; fi;
+          if [ -f "$jupytertxt" ]; then cat $jupytertxt | xargs -n 1 jupyter labextension install --no-build && jupyter lab build --minimize=False; fi;
     preStop:
       exec:
         command:
