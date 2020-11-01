a598b3f62020dad597f6da9b6a9568b45a00c2ef
rushtehrani <r@onepanel.io>
2020-10-31 17:00:38 -0700

    set TENSORBOARD_PROXY_URL so it can be used in JupyterLab

---
+++ db/go/20201031165106_add_tensorboard_env_var_to_jupyterlab_template.go
@@ -0,0 +1,26 @@
+package migration
+
+import (
+	"database/sql"
+	"github.com/pressly/goose"
+)
+
+func initialize20201031165106() {
+	if _, ok := initializedMigrations[20201031165106]; !ok {
+		goose.AddMigration(Up20201031165106, Down20201031165106)
+		initializedMigrations[20201031165106] = true
+	}
+}
+
+// Up20201031165106 updates the jupyterlab workspace to include container lifecycle hooks.
+// These hooks will attempt to persist conda, pip, and jupyterlab extensions between pause and shut-down.
+func Up20201031165106(tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	return updateWorkspaceTemplateManifest("20201031165106_jupyterlab.yaml", jupyterLabTemplateName)
+}
+
+// Down20201031165106 removes the lifecycle hooks from the template.
+func Down20201031165106(tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	return updateWorkspaceTemplateManifest("20201031165106_jupyterlab.yaml", jupyterLabTemplateName)
+}
