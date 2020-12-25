f5320e61a336af3d0c6502cb358ff7971623d9bf
rushtehrani <r@onepanel.io>
2020-12-24 17:19:01 -0800

    update migration date

--- db/go/20201221172926_add_hyperparameter_tuning_workflow_template.go
+++ db/go/20201221172926_add_hyperparameter_tuning_workflow_template.go
@@ -8,18 +8,18 @@ import (
 
 const hyperparameterTuningTemplateName = "Hyperparameter Tuning"
 
-func initialize20201221172926() {
-	if _, ok := initializedMigrations[20201221172926]; !ok {
-		goose.AddMigration(Up20201221172926, Down20201221172926)
-		initializedMigrations[20201221172926] = true
+func initialize20201225172926() {
+	if _, ok := initializedMigrations[20201225172926]; !ok {
+		goose.AddMigration(Up20201225172926, Down20201225172926)
+		initializedMigrations[20201225172926] = true
 	}
 }
 
-// Up20201221172926 adds Hyperparameter Tuning Workflow Template
-func Up20201221172926(tx *sql.Tx) error {
+// Up20201225172926 adds Hyperparameter Tuning Workflow Template
+func Up20201225172926(tx *sql.Tx) error {
 	// This code is executed when the migration is applied.
 	return createWorkflowTemplate(
-		filepath.Join("hyperparam_tuning", "20201221172926.yaml"),
+		filepath.Join("hyperparam_tuning", "20201225172926.yaml"),
 		hyperparameterTuningTemplateName,
 		map[string]string{
 			"framework": "pytorch",
@@ -28,8 +28,8 @@ func Up20201221172926(tx *sql.Tx) error {
 	)
 }
 
-// Down20201221172926 archives Hyperparameter Tuning Workflow Template
-func Down20201221172926(tx *sql.Tx) error {
+// Down20201225172926 archives Hyperparameter Tuning Workflow Template
+func Down20201225172926(tx *sql.Tx) error {
 	// This code is executed when the migration is rolled back.
 	return archiveWorkflowTemplate(hyperparameterTuningTemplateName)
 }
